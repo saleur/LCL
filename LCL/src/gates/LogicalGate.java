d58a1037722ee86e32585784dac2fab8111784e0
saleur <luis.ruelas@upr.edu>
2018-03-19 21:37:31 -0400

    Added some methods to the interface LogicalGate.
    Some eclipse project files where also added. Will document the interface
    LogicalGate at a later time.

--- LCL/src/gates/LogicalGate.java
+++ LCL/src/gates/LogicalGate.java
@@ -1,6 +1,7 @@
 package gates;
 
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Point;
 
 public interface LogicalGate {
@@ -12,6 +13,10 @@ public interface LogicalGate {
 	public Point location();	
 	
 	public Color color();
+	
+	public void update();
+	
+	public void draw(Graphics g);
 	
 
 
