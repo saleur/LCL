c275961cc03155530115b719e3c443c1b9b69a24
Luis <luis.ruelas@upr.edu>
2018-03-29 18:42:06 -0400

    Defined the AbstractLogicalGate class.

--- LCL/src/gates/AbstractLogicalGate.java
+++ LCL/src/gates/AbstractLogicalGate.java
@@ -1,12 +1,38 @@
 package gates;
 
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Point;
 
 public abstract class AbstractLogicalGate implements LogicalGate{
 	
-    protected Color color;
+   
     protected Point location;
+    protected boolean[] inputs;
+    protected boolean output;
+    
+    public AbstractLogicalGate(boolean[] inputs,Point location)
+    {
+    	this.inputs = inputs;
+    	this.location = location;
+    }
+    
+    public boolean output()
+    {
+    	return output;
+    }
+    
+    public boolean[] inputs()
+    {
+    	return inputs;
+    }
+    
+    public abstract void updateOutput(boolean[] newInputs);
+    
+    public abstract void draw(Graphics g);
+    
+    
+    
     
     
 }
