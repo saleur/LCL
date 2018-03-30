821745c5e58634474efbad30d000bbd1b1791de1
Luis <luis.ruelas@upr.edu>
2018-03-30 00:56:24 -0400

    Added ANDGate class.

--- LCL/src/gates/ANDGate.java
+++ LCL/src/gates/ANDGate.java
@@ -3,7 +3,7 @@ package gates;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
-import java.awt.image.ImageObserver;
+
 
 public class ANDGate extends AbstractLogicalGate {
 	
