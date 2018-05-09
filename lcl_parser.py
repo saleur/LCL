78773f35f6b69194ccf560dc30bca357da69ec2f
Luis <luis.ruelas@upr.edu>
2018-05-09 18:44:39 -0400

    Made improvements to parser.
    -At the end of the parsing process only valid code is left.
    -With this valid code, the traduction to the intermidiate Java code can be done.

--- lcl_parser.py
+++ lcl_parser.py
@@ -1,16 +1,21 @@
 
 
+
 from lcl_lexer import tokens
 
-names = { }
+names = { }#Used for verification purposes
+operations = []#At the end it will hold all valid operations.
 
 def p_circuit_assign(p):
     'circuit : NAME EQUALS input'
     names[p[1]] = p[3]
+   
 
 def p_circuit_input(p):
     'circuit : input'
+    print('Printing input!!')
     print(p[1])
+    #print('Printing input!!')
 
 def p_input_operation(p):
     '''input : input AND input
@@ -19,11 +24,16 @@ def p_input_operation(p):
                | input NAND input
                | input NOR  input
                | input XNOR input'''
-    if p[2] == 'and'  : p[0] = p[1] and p[3]
-    elif p[2] == 'or':  p[0] = p[1] or p[3]
-    elif p[2] == 'xor': p[0] = (p[1] and (not p[3])) or ((not p[1]) and p[3]) 
-    elif p[2] == 'nand': p[0] = not(p[1] and p[3])
-    elif p[2] == 'nor': p[0] = not(p[1] or p[3])
+    if p[2] == 'and':
+        p[0] = p[1] and p[3]      
+    elif p[2] == 'or':
+        p[0] = p[1] or p[3]
+    elif p[2] == 'xor':
+        p[0] = (p[1] and (not p[3])) or ((not p[1]) and p[3]) 
+    elif p[2] == 'nand':
+        p[0] = not(p[1] and p[3])
+    elif p[2] == 'nor':
+        p[0] = not(p[1] or p[3])
     elif p[2] == 'xnor': not((p[1] and not p[3]) or (not p[1] and p[3]) )
 
 
@@ -32,19 +42,21 @@ def p_input_factor(p):
     p[0] = p[2]
 
 def p_input_number(p):
-    'input : VALUE'
+    'input : VALUE' 
     p[0] = p[1]
 
 def p_input_name(p):
     'input : NAME'
     try:
-        p[0] = names[p[1]]
+        p[0] = names[p[1]]     
     except LookupError:
         print("Undefined name '%s'" % p[1])
+        del operations[-1]
         p[0] = 0
 
 def p_error(p):
     print("Syntax error at '%s'" % p.value)
+    del operations[-1]
 
 import ply.yacc as yacc
 parser = yacc.yacc()
@@ -58,9 +70,19 @@ try:
       for line in file_object:
           cleanLine = line.strip()
           print(cleanLine)
+          operations.append(cleanLine)
           parser.parse(cleanLine)
-  parser.parse('A')#This verifies that the parser registered the value correctly
-                
+  #Prints out every verified line of code:
+  print('Verified operations')
+  inputs = 'input: '
+  circuits = 'circuit: '
+  for op in operations:
+      if len(op) == 3: 
+          inputs += op + ' '
+      else: 
+          circuits+= op + ' '
+  print(inputs)
+  print(circuits)              
 except IOError:
    print("Error opening file")
    exit()
